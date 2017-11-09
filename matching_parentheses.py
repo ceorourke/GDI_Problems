1aee09fa1b223db63ba38df5a18fae6c1fc7d082
colleen <elena.colleen@gmail.com>
2017-11-09 11:24:05 -0800

    added more test cases and tweaked code for matching parens

--- matching_parentheses.py
+++ matching_parentheses.py
@@ -29,19 +29,25 @@ def has_matching_parentheses(str):
 
     parens = Stack()
     left = {'(', '[', '{'}
-    right = {')', ']', '}'}
+    closed_matches = {')':'(', '}':'{',']':'['}
 
     for char in str:
         if char in left:
             parens.push(char)
-        elif char in right:
+        elif char in closed_matches:
             if parens.is_empty():
                 return False
-            else:
+            # check if closed_matches[char] matches whatever's on top of the stack
+            if parens.peek() == closed_matches[char]: # it's a match, pop it
                 parens.pop()
 
     return parens.is_empty()
 
+str = "{ different parens ]"
+print has_matching_parentheses(str) #=> False
+
+str = "]]] repeated close parens only"
+print has_matching_parentheses(str) #=> False
 
 str = "[({})]"
 print has_matching_parentheses(str) #=> True
