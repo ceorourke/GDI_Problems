b9d8cccc9be78cc1e30083f7bd4143bf2961b973
colleen <elena.colleen@gmail.com>
2017-11-08 16:46:21 -0800

    added morse code tree problem

---
+++ morse_code_tree.py
@@ -0,0 +1,62 @@
+class Node(object):
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return "<Binary Node %s>" % self.data
+
+
+def translate_morse_code(root, morse_code):
+    """Translates morse code to English"""
+
+    result = ""
+
+    for item in morse_code:
+        current = root
+        for char in range(len(item)):
+            if item[char] == ".":
+                current = current.left
+                # print current.data
+            else: 
+                current = current.right
+        result += current.data
+    return result
+
+root = Node("") # morse code tree
+root.left = Node("e")
+root.left.left = Node("i")
+root.left.right = Node("a")
+root.left.right.left = Node("r")
+root.left.right.left.left = Node("l")
+root.left.right.right = Node("w")
+root.left.right.right.left = Node("p")
+root.left.right.right.right = Node("j")
+root.left.left.left = Node("s")
+root.left.left.right = Node("u")
+root.left.left.right.left = Node("f")
+root.left.left.left.left = Node("h")
+root.left.left.left.right = Node("v")
+root.right = Node("t")
+root.right.left = Node("n")
+root.right.left.left = Node("d")
+root.right.left.left.left = Node("b")
+root.right.left.left.right = Node("x")
+root.right.left.right = Node("k")
+root.right.left.right.left = Node("c")
+root.right.left.right.right = Node("y")
+root.right.right = Node("m")
+root.right.right.left = Node("g")
+root.right.right.left.left = Node("z")
+root.right.right.left.right = Node("q")
+root.right.right.right = Node("o")
+
+morse_code = ["....", ".."] #=> "hi"
+print translate_morse_code(root, morse_code)
+morse_code = ["--.","..", ".-.", ".-.."] #=> "girl"
+print translate_morse_code(root, morse_code)
+morse_code = ["--.", "-..", "..", "...", "..-."]
+print translate_morse_code(root, morse_code)
+
+
